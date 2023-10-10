f23926567eee3c59af77f8f13ad666e71ab29be1
Ben Davis <imben123@gmail.com>
2023-10-10 14:55:50 +0100

    Initial Commit

---
+++ BookRabble/BookRabbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookRabbleApp.swift
+//  BookRabble
+//
+//  Created by Ben Davis on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookRabbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
